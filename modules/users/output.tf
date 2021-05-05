c203ebadf7470b0b1f7bfd947c89b2166fdfe4af
Kuldip Sahdeo <kshahdeo50@gmail.com>
2021-05-05 19:22:47 +0530

    Update output.tf

--- modules/users/output.tf
+++ modules/users/output.tf
@@ -2,6 +2,6 @@ output "us_name" {
   value = "${aws_iam_user.user1.*.name}"
 }
 
-output "password" {
-  value = aws_iam_user_login_profile.pass.encrypted_password
-}
+#output "password" {
+#  value = aws_iam_user_login_profile.pass.encrypted_password
+#}
