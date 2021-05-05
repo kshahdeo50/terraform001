c5e043da69f9ddf2edabd5523f2ecd48bac87c8b
Kuldip Sahdeo <kshahdeo50@gmail.com>
2021-05-05 17:27:55 +0530

    Update user.tf

--- modules/users/user.tf
+++ modules/users/user.tf
@@ -2,6 +2,11 @@ resource "aws_iam_user" "user1" {
     name = var.users
 }
 
+resource "aws_iam_user_login_profile" "pass" {
+  user    = aws_iam_user.user1.name
+  pgp_key = "keybase:siba"
+}
+
 
 
 
